a5584ce4e1e0f0e328bfe606496957a3ecec0371
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 14:17:45 -0500

    add end to the instance variable

--- player.rb
+++ player.rb
@@ -1,11 +1,12 @@
 class Player
-  attr_accessor :lives
+  attr_accessor :lives, :name
 
   def initialize(n)
     @name = n
     @lives = 3
   end
 
-  def lose_life 
+  def lose_a_life 
     @lives -= 1
+  end
 end
