ef8fa0d0b2fb81b68da6219af56de81344a073a3
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 14:18:33 -0500

    add game that starts with only one player

---
+++ main.rb
@@ -0,0 +1,31 @@
+require './game'
+require './player'
+require './question'
+
+
+player1 = Player.new("player 1")
+player2 = Player.new("player 2")
+
+game = Game.new(player1, player2)
+
+while player1.lives > 0 && player2.lives > 0 do
+  puts "Let the game begin!"
+  puts "#{player1.name} "
+  
+  # generate a new question with random numbers
+  # and display on screen
+  @question = Question.new
+  @question.new_question
+  
+
+  # get the answer from player and check if correct
+  # if true
+  if @question.check_answer(gets.chomp.to_i)
+    puts "Yup! That's right!"
+  else
+    player1.lose_a_life
+    puts "Nope! better luck next time"
+  end
+  
+end
+
