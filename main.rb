81935bdd694d60866953f0aa3a80382239ed704c
Muna Dahir <muna.dahir@hotmail.com>
2022-01-03 14:26:14 -0500

    add score tally to 1 player game

--- main.rb
+++ main.rb
@@ -10,22 +10,24 @@ game = Game.new(player1, player2)
 
 while player1.lives > 0 && player2.lives > 0 do
   puts "Let the game begin!"
-  puts "#{player1.name} "
+  # puts "#{player1.name} "
   
   # generate a new question with random numbers
   # and display on screen
-  @question = Question.new
-  @question.new_question
+  question = Question.new
+  puts "#{player1.name}: #{question.new_question}"
   
 
-  # get the answer from player and check if correct
-  # if true
-  if @question.check_answer(gets.chomp.to_i)
+  # get the answer from player and check if it's correct
+  if question.check_answer(gets.chomp.to_i)
     puts "Yup! That's right!"
   else
     player1.lose_a_life
     puts "Nope! better luck next time"
   end
+  
+  # show the scoreboard
+  game.score_tally
   
 end
 
